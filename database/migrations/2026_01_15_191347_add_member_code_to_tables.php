b2318866c0fab1c3b44bd1f023fb497edf55ca62
ffatahilah <ffatahilah@bankbjb.co.id>
2026-01-16 15:14:07 +0700

    add membercode

---
+++ database/migrations/2026_01_15_191347_add_member_code_to_tables.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('member_code')->unique()->nullable()->after('id');
+        });
+        
+        Schema::table('memberships', function (Blueprint $table) {
+            $table->string('member_code')->nullable()->after('id');
+        });
+        
+        Schema::table('p_t_members', function (Blueprint $table) {
+            $table->string('member_code')->unique()->nullable()->after('id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn('member_code');
+        });
+        
+        Schema::table('memberships', function (Blueprint $table) {
+            $table->dropColumn('member_code');
+        });
+        
+        Schema::table('p_t_members', function (Blueprint $table) {
+            $table->dropColumn('member_code');
+        });
+    }
+};
